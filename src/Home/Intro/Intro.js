764089327ff53faa88f2a5fc93f412dbfc7abb17
Adam Drummond <adam.drummond9@gmail.com>
2024-09-26 14:13:35 +0100

    added Parallax function to 'Hey, I'm Adam' tex

--- src/Home/Intro/Intro.js
+++ src/Home/Intro/Intro.js
@@ -1,22 +1,29 @@
 import { Col, Container, Row } from "react-bootstrap";
 import './Intro.css';
+import { Parallax } from "react-scroll-parallax";
 
 
 function Intro() {
     return (
-        <Container  className="intro-container dm-sans-500">
+        <Container  className="intro-container dm-sans-500 mt-20">
             <Row>
-                <Col  sm={{span: 8, offset: 2}} className="intro p-3 rounded shadow-sm">
+                <Col  md={{span: 8, offset: 2}} className="intro p-3 rounded shadow-sm">
                     <Row className="align-middle">
-                        <Col md={6} sm={12} className="f-xl text-center d-flex justify-content-center align-items-center">
+                        <Col lg={6} sm={12} className="f-xl text-center d-none d-lg-flex justify-content-center">
+                            <Parallax speed={100} className="d-flex align-items-end parallax-intro">
+                                <span>Hey, I'm Adam!</span>
+                            </Parallax>
+                        </Col>
+                        <Col sm={12} className="d-block d-lg-none f-xl text-center">
                             <span>Hey, I'm Adam!</span>
                         </Col>
-                        <Col md={6} sm={12}>
+                        <Col lg={6} sm={12}>
                             <img src="/img/Adam.png" alt="Adam stood in front of a quay with boats and a lifting bridge" className="img-fluid"/>
                         </Col>
                     </Row>
                 </Col>
             </Row>
+            <div className="high text-center mt-20">(more content coming soon...)</div>
         </Container>
     );
   }
