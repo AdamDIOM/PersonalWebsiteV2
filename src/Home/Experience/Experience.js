3613e56fdbb5b28a7462b101ea083017676910ca
Adam Drummond <adam.drummond9@gmail.com>
2024-09-27 03:03:24 +0100

    added company logos, tidied up loading, adjusted Parallax for Experience title and LinkedIn

--- src/Home/Experience/Experience.js
+++ src/Home/Experience/Experience.js
@@ -10,28 +10,36 @@ function Experience() {
         {
             "company": "Loading...",
             "title": "",
-            "start_date": "",
-            "end_date": " ",
+            "start_month": "",
+            "start_year": "",
+            "end_month": " ",
+            "end_year": "",
             "free_text": "",
             "img_url": "",
-            "type": "exp"
+            "type": "exp",
+            "website_url": ""
           }
     ]);
+
+
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 useEffect(() => {
     fetch('https://script.googleusercontent.com/macros/echo?user_content_key=vPja-qmNR9OACB6PINOgnbHkdZ_FXc2tCG3HD8vosGW6itJaiL9E1hOimu1OR0vxikINgfBUQcNK1BnNE9cY2oZ1n_fjUdQ8m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnE7f5NJmJVFUmIIk6m2PTx-W9nYTVJFE3hBoTHmr7u6lFJHijhv_GG5GwAIBhrZb6-Pm32NKPcw5z7zwJfMr1voe-ZBFab95Fw&lib=M0Dm_mTIH57OLRgr4D2knB9oBno9WCgsY') // Replace with your actual endpoint URL
       .then(response => response.json())
+      .then(response => response.sort((a, b) => months.indexOf(a.start_month) > months.indexOf(b.start_month)))
+      .then(response => response.sort((a, b) => String(b.start_year).localeCompare(String(a.start_year))))
       .then(response => response.sort((a, b) => a.company.localeCompare(b.company)))
       .then(data => setData(data))
       .then(console.log(data))
       .catch(error => console.error('Error fetching data:', error));
-  });
+  }, [""]);
 
 
     return (
         <Col  md={8} className="experience p-3 rounded shadow-sm mt-10 mb-5 dm-sans-500">
             <Row className="align-middle">
-                <Col lg={6} sm={12} className="f-xl text-center d-none d-lg-flex justify-content-center">
+                <Col lg={6} sm={12} className="f-xl text-center d-none d-lg-flex justify-content-center align-items-center">
                     <Parallax speed={100} className="d-flex parallax-experience">
                         <h1>Experience</h1>
                     </Parallax>
@@ -40,27 +48,32 @@ useEffect(() => {
                     <h1>Experience</h1>
                 </Col>
                 <Col lg={6} sm={12}>
-                    {data.map((item, index, list) => (
-                        item !== undefined && item.type !== undefined && item.type !== "exp" ? <React.Fragment /> : 
-                        list[index-1] === undefined ? (
-                            <React.Fragment>
-                                <h2>{item.company}</h2>
-                                <b>{item.title}</b>
-                                <p>{item.start_date} - {item.end_date !== "" ? item.end_date : "Present"}</p>
-                            </React.Fragment>
-                        ) :
-                        list[index-1] !== undefined && list[index-1].company !== list[index].company ? (
-                            <React.Fragment>
-                                <h2>{item.company}</h2>
-                                <b>{item.title}</b>
-                                <p>{item.start_date} - {item.end_date !== "" ? item.end_date : "Present"}</p>
-                            </React.Fragment>
-                        )
-                        :
-                        <React.Fragment>
-                            <b>{item.title}</b>
-                            <p>{item.start_date} - {item.end_date !== "" ? item.end_date : "Present"}</p>
-                        </React.Fragment>
+
+                    {data.filter((item) => (item.type === "exp"))
+                    .map((item, index, list) => (
+                        <Row key={index}>
+                            
+                        {((list[index-1] === undefined) || (list[index-1] !== undefined && list[index-1].company !== list[index].company)) && item.img_url !== undefined && item.img_url !== "" ? (
+                            <h2>
+                                {item.website_url !== null && item.website_url !== "" ? (
+                                    <a href={item.website_url} target="_blank" rel="noreferrer"><img src={item.img_url} alt={item.company + "logo"} className="w-100 pe-5" /></a>
+                                ) : (
+                                    <img src={item.img_url} alt={item.company + "logo"} className="w-100 pe-5" />
+                                )}
+                            </h2>
+                            ) : (list[index-1] === undefined) || (list[index-1] !== undefined && list[index-1].company !== list[index].company) ? (
+                                <h2>
+                                    {item.website_url !== null && item.website_url !== "" ? (
+                                    <a href={item.website_url} target="_blank" rel="noreferrer" className="text-decoration-none text-dark">{item.company}</a>
+                                ) : (
+                                    <React.Fragment>{item.company}</React.Fragment>
+                                )}
+                                </h2>
+                             ) : ""}
+                        <b>{item.title}</b>
+                        <p>{(item.start_year !== undefined && item.start_year !== "") ? (<React.Fragment>{item.start_month} {item.start_year} - {item.end_month !== "" ? `${item.end_month} ${item.end_year}` : "Present"}</React.Fragment>) : ""}</p>
+                        {(list[index+1] !== undefined && list[index+1].company !== list[index].company) ? <hr />: ""}
+                    </Row>
                     ))}
                 </Col>
             </Row>
--- src/Home/LinkedIn/LinkedIn.js
+++ src/Home/LinkedIn/LinkedIn.js
@@ -6,7 +6,7 @@ import { Parallax } from "react-scroll-parallax";
 function LinkedIn() {
 
     return (
-        <Col  md={{span:2, offset:1}} className="d-flex">
+        <Col  md={{span:2, offset:1}} className="d-flex align-items-center">
             <Parallax speed={100} className="d-none d-md-flex parallax-experience">
                 <a href="https://linkedin.com/in/adam-drummond">
                 <Row className="linkedin p-3 rounded shadow-sm mt-10 mb-2 dm-sans-500 h-25 d-flex align-items-center justify-content-center h-auto">
