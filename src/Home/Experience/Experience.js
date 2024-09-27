93ad0f7db5adcf797191267903fe6ae50d4678c6
Adam Drummond <adam.drummond9@gmail.com>
2024-09-27 03:08:31 +0100

    modified useEffect hook to fix dependencies issue for Netlify

--- src/Home/Experience/Experience.js
+++ src/Home/Experience/Experience.js
@@ -22,9 +22,10 @@ function Experience() {
     ]);
 
 
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 useEffect(() => {
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    function fetchData() {
     fetch('https://script.googleusercontent.com/macros/echo?user_content_key=vPja-qmNR9OACB6PINOgnbHkdZ_FXc2tCG3HD8vosGW6itJaiL9E1hOimu1OR0vxikINgfBUQcNK1BnNE9cY2oZ1n_fjUdQ8m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnE7f5NJmJVFUmIIk6m2PTx-W9nYTVJFE3hBoTHmr7u6lFJHijhv_GG5GwAIBhrZb6-Pm32NKPcw5z7zwJfMr1voe-ZBFab95Fw&lib=M0Dm_mTIH57OLRgr4D2knB9oBno9WCgsY') // Replace with your actual endpoint URL
       .then(response => response.json())
       .then(response => response.sort((a, b) => months.indexOf(a.start_month) > months.indexOf(b.start_month)))
@@ -33,7 +34,9 @@ useEffect(() => {
       .then(data => setData(data))
       .then(console.log(data))
       .catch(error => console.error('Error fetching data:', error));
-  }, [""]);
+    }
+    fetchData()
+  }, []);
 
 
     return (
