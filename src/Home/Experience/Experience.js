be78e6ede47b4b0e538d28de332186c0ba38f9c6
Adam Drummond <adam.drummond9@gmail.com>
2024-09-27 18:42:28 +0100

    removed unused variable

--- src/Home/Experience/Experience.js
+++ src/Home/Experience/Experience.js
@@ -41,7 +41,6 @@ useEffect(() => {
     }
 
     function fetchData() {
-        var received = [];
         fetch('https://script.googleusercontent.com/macros/echo?user_content_key=vPja-qmNR9OACB6PINOgnbHkdZ_FXc2tCG3HD8vosGW6itJaiL9E1hOimu1OR0vxikINgfBUQcNK1BnNE9cY2oZ1n_fjUdQ8m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnE7f5NJmJVFUmIIk6m2PTx-W9nYTVJFE3hBoTHmr7u6lFJHijhv_GG5GwAIBhrZb6-Pm32NKPcw5z7zwJfMr1voe-ZBFab95Fw&lib=M0Dm_mTIH57OLRgr4D2knB9oBno9WCgsY') 
         .then(response => response.json())
         .then(response => response.sort((a, b) => months.indexOf(a.start_month) > months.indexOf(b.start_month)))
